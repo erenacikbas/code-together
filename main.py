bd379cead074a39fc8ff797c91338dd8f78bdd7c
Eren Tuna AÇIKBAŞ <erentuna26@icloud.com>
2021-01-12 15:09:15 +0300

    small updates and configurations

--- main.py
+++ main.py
@@ -1,55 +1,63 @@
-# 1. Web Scraping
 # importing beautifulsoup
 from bs4 import BeautifulSoup
 import ssl
 import urllib.request
 
+# Taking Inputs
 
-def web_scraping():
+
+def input_grabber():
+    links = []
+    i = 0
+    while i < 2:
+        link = str(input("Enter Book " + str((i+1)) + " Link: "))
+        links.append(dict({"Book " + str(i+1) + ".txt": link}))
+        i += 1
+    return links
+
+
+# 1. Web Scraping
+
+def web_scraping(link):
+    book = ""
+    bookLink = ""
+    for key, value in link.items():
+        book = key
+        bookLink = value
 
     # python 2.6
-    file1 = open("python2.6.txt", "a")
-    file1.clear()
-    # python 3
-    file2 = open("python3.txt", "a")
-    file2.clear()
+    file1 = open(book, "a")
+    file1.truncate(0)
 
     context = ssl._create_unverified_context()
 
-    link1 = urllib.request.urlopen(
-        "https://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_2.6/Print_version", context=context)
-
-    link2 = urllib.request.urlopen(
-        "https://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_3/Print_version", context=context)
+    link = urllib.request.urlopen(
+        bookLink, context=context)
 
     # Link 1 Soup
-    soupForLink1 = BeautifulSoup(link1, 'html.parser')
-
-    # Link 2 Soup
-    soupforLink2 = BeautifulSoup(link2, 'html.parser')
-
-    # # Header Writer Subtitle
-    # soup2 = BeautifulSoup(open('sample.html'), 'html.parser')
-    # # Text
-    # soup = BeautifulSoup(u, 'html.parser')
+    soupForLink = BeautifulSoup(link, 'html.parser')
 
     Lines = [
         # Text
-        soupForLink1.find(id='bodyContent').get_text()
+        soupForLink.find(id='bodyContent').get_text()
     ]
-    # file operations - 2.6
     file1.writelines(Lines)
     file1.close()
-
-    # file2 operations - 3.0
-    file2.write(soupforLink2.find(id="mw-content-text").get_text())
-    file2.close()
+    return book
 
 
 # 2. Stripping
 
-def stripping():
-    pass
+def stripping(bookName):
+    book = open(bookName, "r+")
+    stripped_book = open("Stripped_" + bookName, "a")
+    text = book.read()
+    stripped_text = " ".join(text.split())
+    stripped_book.write(stripped_text.casefold())
+    stripped_book.close()
+    book.close()
+
+    # file operations for book1
 
 
 # 3. Word Counting
@@ -59,10 +67,13 @@ def word_counting():
 
 # 4. Test
 
+
 def test():
-    web_scraping()
-    stripping()
-    word_counting()
+    links = input_grabber()
+    for link in links:
+        book = web_scraping(link)
+        stripping(book)
+        # word_counting()
+
 
 test()
-
