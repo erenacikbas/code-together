628056ea8e839a33904be8aab1edc3eab9326c97
Abdullah DİNÇ <abdullahdinc14@hotmail.com>
2021-01-17 09:45:36 +0300

    fixes done

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from collections import Counter
 
 # Unwanted Word Lists
 unwanted_words = [">>>", "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against",
-                  "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "==", "+", "=", "-"]
+                  "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "==", "=", "-","1","2","3","4","5","6","7","8","9","0","*","(",")","_","'","-","=","[","]",",","[]","←","()","(print(","print()","#"]
 
 
 # Taking Inputs
@@ -16,7 +16,10 @@ def input_grabber():
     links = []
     i = 0
     while i < 2:
-        link = str(input("Enter Book " + str((i+1)) + " Link: "))
+        givenbook = str(input("Enter Book " + str((i+1)) + " Name: ")) 
+        givenbook=givenbook.replace(" ", "_")
+        givenbook=givenbook.replace("'","%27" ) 
+        link=("https://en.wikibooks.org/wiki/"+(givenbook)+"/Print_version")
         links.append(dict({"Book " + str(i+1) + ".txt": link}))
         i += 1
     return links
@@ -52,12 +55,17 @@ def web_scraping(link):
 
 # 2. Stripping
 
+        
 def stripping(bookName):
     book = open(bookName, "r+")
     stripped_book_name = "Stripped_" + bookName
     stripped_book = open(stripped_book_name, "a")
     stripped_book.truncate(0)
     text = book.read()
+    
+    # for a in unwanted_words:
+    #     if(a in text):
+    #         text=text.replace("a"," ")
     stripped_text = " ".join(text.split())
     stripped_book.write(stripped_text.casefold())
     stripped_book.close()
@@ -69,11 +77,11 @@ def stripping(bookName):
 
 
 # 3. Word Counting
-
 def word_counting(bookName, stripped_book):
     sorted_dict = {}
+    
     stripped_book_text = open(stripped_book, "r+").read()
-
+    
     def pretty(d, indent=0):
         for key, value in d.items():
             print('\t' * indent + str(key))
@@ -86,6 +94,7 @@ def word_counting(bookName, stripped_book):
         words = str.split()
         counts = dict()
         sorted_text = ""
+    
         for word in words:
             if word in counts:
                 counts[word] += 1
@@ -100,7 +109,7 @@ def word_counting(bookName, stripped_book):
         # Removing Unwanted Items
         for uw_word in unwanted_words:
             if(uw_word in sorted_dict):
-                del sorted_dict[uw_word]
+                  del sorted_dict[uw_word]
         sorted_dic_items = sorted_dict.items()
         first_20_words = list(sorted_dic_items)[:20]
         print(first_20_words)
@@ -120,7 +129,19 @@ def word_counting(bookName, stripped_book):
 # 5. Finding Intersection Points
 
 
-def common_elements(sorted_dictionaries):
+     
+def intersect_elements(sorted_dictionaries):
+    dict__1 = sorted_dictionaries[0]
+    dict__2 = sorted_dictionaries[1]
+
+    ortak={}
+     #intersects
+    for eleman in dict__1.keys():
+         if eleman in dict__2.keys():
+            ortak[eleman]=(dict__1["eleman"])
+            
+    print("ortaklar: " + str(list(ortak.items())[:20]) + "\n\n")
+    
     dict_1 = sorted_dictionaries[0]
     dict_2 = sorted_dictionaries[1]
 
@@ -129,6 +150,7 @@ def common_elements(sorted_dictionaries):
     for item in dict_1.keys():
         if item in dict_2.keys():
             intersects.append(item)
+
     # print("Intersects: ", intersects)
     # commons
     for dictionary in sorted_dictionaries:
@@ -136,9 +158,15 @@ def common_elements(sorted_dictionaries):
             if(intersect in dictionary):
                 del dictionary[intersect]
     print("---------------------------------")
-    print("Common Words")
+    print("Intersect Words")
     print("dict_1 : " + str(list(dict_1.items())[:20]) + "\n\n")
     print("dict_2 : " + str(list(dict_2.items())[:20]))
+
+
+            
+
+
+
 
 # 6. Test
 
@@ -149,10 +177,12 @@ def test():
     sorted_dictionaries = []
     for link in links:
         book = web_scraping(link)
+        
         stripped_book = stripping(book)
         sorted_dict = word_counting(book, stripped_book)
         sorted_dictionaries.append(sorted_dict)
-    common_elements(sorted_dictionaries)
+    intersect_elements(sorted_dictionaries)
+    
 
 
 test()
